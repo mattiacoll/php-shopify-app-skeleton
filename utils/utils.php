6a57af8c4d6664200dd8bf1809afd0893a1fa75a
mattiacoll <64754556+mattiacoll@users.noreply.github.com>
2021-11-27 00:21:22 +0100

    Improved index functions

--- utils/utils.php
+++ utils/utils.php
@@ -2,7 +2,7 @@
 
 include_once 'config.php';
 
-function verifyHMAC(){
+function verifyHMAC() {
 
   global $s;
   global $message;
@@ -70,88 +70,53 @@ function storeNonce( $client_id, $nonce ) {
 }
 
 //UA - User Authentication
-function getClientId($shop){
-
-  global $sn, $dn, $un, $pw;
+function getClientId( $shop ) {
 
   $client_id = -1;
 
-  $dsn = "mysql:host=".$sn.";dbname=".$dn.";charset=utf8";
-  $opt = array(
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-  );
+  try {
 
-  $return_check = $pdo = new PDO($dsn, $un, $pw, $opt);
+    $pdo;
+    connect_db( $pdo );
 
-  if($return_check === false){
-    echo "Unable to process request. ERROR: I-GCI-1";
-    die();
-  }
-  $return_check = $stm = $pdo->prepare("SELECT client_id FROM clients WHERE client_name = ?");
+    $stm = $pdo->prepare( 'SELECT client_id FROM clients WHERE client_name = ?' );
+    $stm->execute(array($shop));
 
-  if($return_check === false){
-    echo "Unable to process request. ERROR: I-GCI-2";
-    die();
-  }
-  $return_check = $stm->execute(array($shop));
+    $result = $stm->fetchAll();
 
-  if($return_check === false){
-    echo "Unable to process request. ERROR: I-GCI-3";
-    die();
-  }
-  $result = $stm->fetchAll();
+    if ( count( $result ) )
+      $client_id = $result[0]['client_id'];
 
-  if(count($result) !== 0){
-    $client_id = $result[0]['client_id'];
-  }
-  else{
-    echo "Unable to process request. ERROR: I-GCI-4";
-    die();
+    $pdo = null;
+
+  } catch ( PDOException $err ) {
+    die( 'Unable to process request. ' . $err->getMessage() );
   }
 
   return $client_id;
 }
 
-function verifyHMACClient($hmac, $client_id){
+function verifyHMACClient( $hmac, $client_id ) {
 
-  global $sn, $dn, $un, $pw, $s;
-      
-  $dsn = "mysql:host=".$sn.";dbname=".$dn.";charset=utf8";
-  $opt = array(
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-  );
+  try {
 
-  $return_check = $pdo = new PDO($dsn, $un, $pw, $opt);
-  if($return_check === false){
-    die("Unable to process request. ERROR: I-VH-1");
-  }
-  $return_check = $stm = $pdo->prepare("SELECT nonce FROM client_stores WHERE client_id = ? AND last_activity >= NOW() - INTERVAL 10 SECOND AND active = 1");
-  if($return_check === false){
-    die("Unable to process request. ERROR: I-VH-2");
-  }
-  $return_check = $stm->execute(array($client_id));
+    $pdo;
+    connect_db( $pdo );
 
-  if($return_check === false){
-    die("Unable to process request. ERROR: I-VH-3");
-  }
+    $stm = $pdo->prepare( 'SELECT nonce FROM client_stores WHERE client_id = ? AND last_activity >= NOW() - INTERVAL 10 SECOND AND active = 1' );
+    $stm->execute([ $client_id ]);
 
-  $result = $stm->fetchAll();
+    $result = $stm->fetchAll();
 
-  if(count($result) !== 0){
-    $nonce = $result[0]['nonce'];
-  }
-  else{
-    die("Unable to process request. ERROR: I-VH-4");
+    if ( count( $result ) )
+      $nonce = $result[0]['nonce'];
+
+    $check = hash_hmac( 'sha256', $nonce, $s );
+
+    return ( $check === $hmac );
+
+  } catch ( PDOException $err ) {
+    die( 'Unable to process request. ' . $err->getMessage() );
   }
 
-  $check = hash_hmac('sha256', $nonce, $s);
-
-  if($check == $hmac){
-    return true;
-  }
-  else{
-    return false;
-  }
 }
